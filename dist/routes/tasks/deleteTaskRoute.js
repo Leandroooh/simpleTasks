26404452f839d72ee5ae15d955e5b99d7511c1ca
leeandrooh <ls@dev.com>
2025-12-01 09:55:11 -0300

    feat: fix build imports

--- dist/routes/tasks/deleteTaskRoute.js
+++ dist/routes/tasks/deleteTaskRoute.js
@@ -1,35 +1,39 @@
 import z from "zod";
-import { AuthHandler } from "@/middlewares/AuthHandler.js";
-import { prisma } from "@/prisma/client.js";
+import { AuthHandler } from "../../middlewares/AuthHandler.js";
+import { prisma } from "../../prisma/client.js";
 export async function DeleteTaskRoute(app) {
-    app
-        .withTypeProvider()
-        .register(AuthHandler)
-        .delete("/task/delete/:id", {
-        schema: {
-            tags: ["Task"],
-            params: z.object({
-                id: z.string(),
-            }),
-        },
-    }, async (request, reply) => {
-        const { id } = request.params;
-        const userId = await request.getCurrentUserToken();
-        const findTaskById = await prisma.task.findFirst({
-            where: {
-                id,
-                userId,
-            },
-        });
-        if (!findTaskById) {
-            return reply.status(404).send({ message: "Task not found!" });
-        }
-        const deletedTask = await prisma.task.delete({
-            where: {
-                id,
-                userId,
-            },
-        });
-        return reply.status(200).send(deletedTask);
-    });
+	app
+		.withTypeProvider()
+		.register(AuthHandler)
+		.delete(
+			"/task/delete/:id",
+			{
+				schema: {
+					tags: ["Task"],
+					params: z.object({
+						id: z.string(),
+					}),
+				},
+			},
+			async (request, reply) => {
+				const { id } = request.params;
+				const userId = await request.getCurrentUserToken();
+				const findTaskById = await prisma.task.findFirst({
+					where: {
+						id,
+						userId,
+					},
+				});
+				if (!findTaskById) {
+					return reply.status(404).send({ message: "Task not found!" });
+				}
+				const deletedTask = await prisma.task.delete({
+					where: {
+						id,
+						userId,
+					},
+				});
+				return reply.status(200).send(deletedTask);
+			},
+		);
 }
